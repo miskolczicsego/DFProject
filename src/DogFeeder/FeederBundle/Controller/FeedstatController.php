19f6a4da664afc65adf7befe93e8369aeee7f3ca
miskolczicsego <miskolczicsego@webshopexperts.hu>
2017-03-19 22:45:08 +0100

    -small correction on how to organizing files, and datas

--- src/DogFeeder/FeederBundle/Controller/FeedstatController.php
+++ src/DogFeeder/FeederBundle/Controller/FeedstatController.php
@@ -37,4 +37,27 @@ class FeedstatController extends Controller
             'getLastFeedstatsByUserId' => $lastfeedstat,
         ));
     }
+
+    public function filterAction(Request $request)
+    {
+        $feederName = $request->get('name');
+
+        $optionValue = $request->get('value');
+
+        $config = $this->get('config');
+        $statLimit = $config->get('stat_limit', $this->getUser()->getId())->getValue();
+        if ($optionValue != false) {
+
+            $filteredStats = $this->getDoctrine()->getManager()->getRepository('FeederBundle:FeedStat')->findStatsByFeeder($feederName, $statLimit);
+            return $this->render('@Feeder/FeedstatTable/feedstat_table.html.twig', array(
+                'getLastFeedstatsByUserId' => $filteredStats
+            ));
+        }
+
+        $allStats = $this->getDoctrine()->getManager()->getRepository('FeederBundle:FeedStat')->getLastFeedstatsByUserId($this->getUser()->getId(), $statLimit);
+        return $this->render('@Feeder/FeedstatTable/feedstat_table.html.twig', array(
+            'getLastFeedstatsByUserId' => $allStats
+        ));
+
+    }
 }
